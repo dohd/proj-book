3d2f996c076d9853215cf4149bd3d39fece80392
Dohd <alloicebrian@gmail.com>
2021-06-03 18:15:22 +0300

    Add catch on promise.reject

--- src/api/errorHandler.js
+++ src/api/errorHandler.js
@@ -1,11 +1,8 @@
 import { message } from 'antd';
 
 const errorHandler = data => {
-    if (data.error) {
-        console.log(data.error);
-        message.error(data.error.message);
-    }
-    return Promise.reject(data);
+    if (data.error) message.error(data.error.message);    
+    return Promise.reject(data).catch(console.log);
 };
 
 export default errorHandler;
