4f3bed2a6f465857f1ad619b0f5d49c6a49360a8
Dohd <alloicebrian@gmail.com>
2021-06-20 20:02:46 +0300

    enable ssl on server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.enable('trust proxy');
+app.use((req, res, next) => (
+  req.secure ? next() : res.redirect(`https://${req.headers.host}${req.url}`)
+));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Server connected on port:', port));
