c3b88c2a255a982624bc85682fb8f94e063900bf
Dohd <alloicebrian@gmail.com>
2021-07-03 16:25:58 +0300

    Refactor upload function

--- src/pages/report_image/ReportImageContainer.jsx
+++ src/pages/report_image/ReportImageContainer.jsx
@@ -38,26 +38,21 @@ export default function ReportImageContainer() {
     const [loading, setLoading] = useState(false);
 
     const upload = async file => {
-        try {
-            const url = await uploadTask(`/events/${file.name}`, file);
-            const values = { narrativeReportId, url };
-            const res = await Api.eventImage.post(values);
-            if (res) fetchNarratives(dispatch);
-            setLoading(false);    
-        } catch (error) {
-            console.log(error);
-        }
+        const url = await uploadTask(`/events/${file.name}`, file);
+        const values = { narrativeReportId, url };
+        const res = await Api.eventImage.post(values);
+        if (res) fetchNarratives(dispatch);
+        setLoading(false);    
     };
 
     const handleBeforeUpload = file => {
         const name = `${fetchAud()}-${narrativeReportId}-${file.name}`;
         const renamedFile = new File([file], name, {type: file.type});
-
         setLoading(true);
-        upload(renamedFile);
+        upload(renamedFile).catch(console.log);
         return false;
     };
 
     const props = { handleBeforeUpload, loading, eventImages };
-    return <ReportImage {...props} />
+    return <ReportImage {...props} />;
 }
