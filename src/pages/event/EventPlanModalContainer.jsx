cd020eafffb529d40f652dcde7fe41be533a686f
Dohd <alloicebrian@gmail.com>
2021-09-10 10:37:17 +0300

    Update useEffect

--- src/pages/event/EventPlanModalContainer.jsx
+++ src/pages/event/EventPlanModalContainer.jsx
@@ -9,51 +9,28 @@ export default function EventPlanModalContainer(props) {
         eventDate, ...rest
     } = props;
 
+    const [plans, setPlans] = useState([]);
+    useEffect(() => {
+        if (activityPlans.length) {
+            const plans = activityPlans.map(obj => ({
+                key: obj.id,
+                activity: obj.action,
+                programme: obj.programme,
+                region: obj.region.join(', '),
+                group: obj.group.join(', '),
+                material: obj.material,
+                status: obj.status
+            }));
+            setPlans(plans);
+        }
+    }, [activityPlans]);
+
     const onCancel = () => setVisible(false);
     const onOk = () => setVisible(false);
 
-    const [plans, setPlans] = useState([]);
-    useEffect(() => {
-        const list = activityPlans.map(obj => {
-            const plan = { 
-                key: obj.id,
-                activityId: obj.activity.id,
-                activity: obj.activity.action,
-                programme: obj.planProgramme.keyProgramme.programme,
-            };
-
-            const regions = new Set();
-            obj.planEvents.forEach(({planRegions}) => {
-                planRegions.forEach(({region}) => {
-                    regions.add(region.area);
-                });
-            });
-
-            const groups = new Set();
-            obj.planGroups.forEach(({targetGroup}) => {
-                groups.add(targetGroup.group);
-            });
-
-            let status = '';
-            for (const p of participants) {
-                if (p.activityId === plan.activityId) {
-                    status = 'Executed';
-                    break;
-                }
-            }
-
-            plan.regions = [...regions];
-            plan.groups = [...groups];
-            plan.materials = obj.planMaterial.material;
-            plan.status = status;
-            return plan;
-        });
-        setPlans(list);
-    }, [activityPlans, participants]);
-
     const onExport = () => {
         const cells = [];
-        plans.forEach(({key, activityId, ...rest}) => {
+        plans.forEach(({key, ...rest}) => {
             for (const key in rest) {
                 const v = rest[key];
                 if (Array.isArray(v)) cells.push({text: v.join(', ')});
