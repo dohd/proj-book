28f2fa2f855b4fbe222f41e80911608efe6fe4e1
Dohd <alloicebrian@gmail.com>
2021-09-15 15:09:50 +0300

    Add div wrapper to ActivityReport table

--- src/pages/activity_report/ActivityReport.jsx
+++ src/pages/activity_report/ActivityReport.jsx
@@ -14,8 +14,7 @@ export default function ActivityReport(props) {
     
     return (
         <Card
-            title='Activity Reports'
-            style={{overflowX: 'auto'}}
+            title='Activity Reports'            
             bordered={false}
             extra={
                 <Button 
@@ -38,51 +37,53 @@ export default function ActivityReport(props) {
                 record={record}
             />
 
-            <Table 
-                dataSource={activities}
-                columns={[
-                    {
-                        title: 'Activity',
-                        dataIndex: 'activity',
-                        key: 'activity'
-                    },
-                    {
-                        title: 'Date',
-                        dataIndex: 'date',
-                        key: 'date'
-                    },
-                    {
-                        title: 'Programme',
-                        dataIndex: 'programme',
-                        key: 'programme'
-                    },
-                    {
-                        title: 'Region',
-                        dataIndex: 'region',
-                        key: 'region'
-                    },
-                    {
-                        title: 'Group',
-                        dataIndex: 'group',
-                        key: 'group'
-                    },
-                    {
-                        title: 'Report',
-                        key: 'report',
-                        render: (text, {narratives}) => {
-                            return (
-                                <Button 
-                                    type='link' 
-                                    onClick={() => showReportModal(narratives)}
-                                    size='large'
-                                >
-                                    report
-                                </Button>
-                            );
+            <div style={{overflowX: 'auto'}}>
+                <Table 
+                    dataSource={activities}
+                    columns={[
+                        {
+                            title: 'Activity',
+                            dataIndex: 'activity',
+                            key: 'activity'
+                        },
+                        {
+                            title: 'Date',
+                            dataIndex: 'date',
+                            key: 'date'
+                        },
+                        {
+                            title: 'Programme',
+                            dataIndex: 'programme',
+                            key: 'programme'
+                        },
+                        {
+                            title: 'Region',
+                            dataIndex: 'region',
+                            key: 'region'
+                        },
+                        {
+                            title: 'Group',
+                            dataIndex: 'group',
+                            key: 'group'
+                        },
+                        {
+                            title: 'Report',
+                            key: 'report',
+                            render: (text, {narratives}) => {
+                                return (
+                                    <Button 
+                                        type='link' 
+                                        onClick={() => showReportModal(narratives)}
+                                        size='large'
+                                    >
+                                        report
+                                    </Button>
+                                );
+                            }
                         }
-                    }
-                ]}
-            />
+                    ]}
+                />
+            </div>
         </Card>
     );
 }
