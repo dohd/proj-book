485b60085ec0a049d76ef095a201fbd99c6f93f1
Dohd <alloicebrian@gmail.com>
2021-10-12 23:51:16 +0300

    Update ResponseContainer taskCols

--- src/pages/activity_report/ResponseContainer.jsx
+++ src/pages/activity_report/ResponseContainer.jsx
@@ -11,35 +11,61 @@ export default function ResponseContainer(props) {
 
     const record = respState?.record;
     const {query, response, task, response_id} = record.response[0];
-
-    // task columns
-    const taskCol = [...new Set(task)].map((v,i) => ({
-        title: v, key: i, dataIndex: v 
-    }));
-    // response rows
-    const queryObj = query.reduce((r,c,i) => {
-        if (!r[c]) {
-            r[c] = {
+    /**
+     * Response object map
+     * 
+     * @var query object
+     * @var response object
+     * @var task object
+     * @var response_id object
+     */
+    const queryObj = query.reduce((acc, curr, i) => {
+        if (!acc[curr]) {
+            acc[curr] = {
                 key: i,
-                query: c,
+                query: curr,
                 [task[i]]:response[i],
-                [`${task[i]}_id`]: response_id[i]
+                [`${task[i]}_id`]: response_id[i],
             };
-        } else {
-            const prev = r[c];
-            r[c] = {
-                ...prev,
-                [task[i]]:response[i],
-                [`${task[i]}_id`]: response_id[i]
-            };
+            return acc;
         }
-        return r;
+        const val = acc[curr];
+        acc[curr] = {
+            ...val,
+            [task[i]]:response[i],
+            [`${task[i]}_id`]: response_id[i]
+        };        
+        return acc;
     }, {});
+    // Response rows
     const rowData = Object.values(queryObj);
 
+    // add editable property to task column objects
+    const taskCols = [...new Set(task)].map((v, i) => ({
+        title: v, 
+        key: i, 
+        dataIndex: v,
+        editable: true,
+    }));
+
+    // add oncell function to editable column object
+    const columns = taskCols.map((col) => {
+        if (!col.editable) return col;
+        return {
+            ...col,
+            onCell: (record) => ({
+                record,
+                editable: col.editable,
+                dataIndex: col.dataIndex,
+                title: col.title,
+            }),
+        };
+    });
+
     const params = {
-        toggleReportView, taskCol, rowData, 
-        case_study: record.case_study
+        toggleReportView, rowData, 
+        case_study: record.case_study,
+        taskCols: columns,
     };
     return <Response {...params} />;
 }
