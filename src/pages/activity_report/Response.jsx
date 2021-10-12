616fdab58948efce412b659acb8975d68e1616a1
Dohd <alloicebrian@gmail.com>
2021-10-12 23:49:22 +0300

    Import components and update table props

--- src/pages/activity_report/Response.jsx
+++ src/pages/activity_report/Response.jsx
@@ -1,12 +1,14 @@
 import React, {  } from 'react';
 import { Button, Card, Popconfirm, Space, Table } from 'antd';
-import { 
-    ArrowLeftOutlined, FilePdfOutlined, EditTwoTone, 
-    DeleteOutlined 
-} from '@ant-design/icons';
+import { ArrowLeftOutlined, FilePdfOutlined, DeleteOutlined } from '@ant-design/icons';
+
+import components from './EditableTable';
 
 export default function Response(props) {
-    const {toggleReportView, taskCol, rowData, case_study} = props;
+    const {
+        toggleReportView, taskCols, rowData, case_study,
+    } = props;
+
     return (
         <Card
             bordered={false}
@@ -32,55 +34,45 @@ export default function Response(props) {
         >
             <div>
                 <p>Case study</p>
-                <p>&bull;&nbsp;
-                    {case_study}
-                </p>
+                <p>&bull;&nbsp;{case_study}</p>
             </div>
             
             <div style={{overflowX: 'auto'}}>
                 <Table
+                    components={components}
+                    rowClassName={() => 'editable-row'}
                     dataSource={rowData}
                     columns={[
                         {
                             title: 'Question',
                             key: 'query',
-                            dataIndex: 'query'
+                            dataIndex: 'query',
                         },
                         {
                             title: 'Task Response',
-                            children: taskCol
+                            children: taskCols
                         },
                         {
                             title: 'Action',
                             dataIndex: 'action',
                             key: 'action',
                             render: (text, record) => {
-                                return (
-                                    <Space>
-                                        <Button 
-                                            type='link' 
-                                            // onClick={() => showUpdateModal(record)}
+                                return (                               
+                                    <Popconfirm
+                                        title='Are you sure to delete this response?'
+                                        // onConfirm={() => onDelete(record.key)}
+                                        okText='Yes'
+                                        cancelText='No'
+                                    >
+                                        <Button
+                                            type='link'
                                             icon={
-                                                <EditTwoTone style={{ fontSize: '20px' }} />
+                                                <DeleteOutlined 
+                                                    style={{color: 'red', fontSize: '18px'}} 
+                                                />
                                             }
                                         />
-                                        
-                                        <Popconfirm
-                                            title='Are you sure to delete this donor?'
-                                            // onConfirm={() => onDelete(record.key)}
-                                            okText='Yes'
-                                            cancelText='No'
-                                        >
-                                            <Button
-                                                type='link'
-                                                icon={
-                                                    <DeleteOutlined 
-                                                        style={{ color: 'red', fontSize: '18px' }} 
-                                                    />
-                                                }
-                                            />
-                                        </Popconfirm>
-                                    </Space>
+                                    </Popconfirm>                                    
                                 );
                             }
                         }
