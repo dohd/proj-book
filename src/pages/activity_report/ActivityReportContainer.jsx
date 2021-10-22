c55d501ab0274fc38004cc14bf7417617952281c
Dohd <alloicebrian@gmail.com>
2021-10-22 22:12:01 +0300

    Add useEffect state persistance

--- src/pages/activity_report/ActivityReportContainer.jsx
+++ src/pages/activity_report/ActivityReportContainer.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useTracked } from 'context';
+import Api from 'api';
+import { clientSocket } from 'utils';
 
 import ActivityReport from './ActivityReport';
 import ResponseContainer from './ResponseContainer';
 import ReportImageContainer from './ReportImageContainer';
 
-export default function ActivityReportContainer() {
-    const store = useTracked()[0];
+const fetchNarratives = async dispatch => {
+    const narratives = await Api.narrative.get()
+    dispatch({type: 'addNarratives', payload: narratives});
+    clientSocket.emit('narratives', narratives);
+}
 
-    const [respState, setRespState] = useState({
+export default function ActivityReportContainer() {
+    const [store, dispatch] = useTracked();
+    const {narratives: activities} = store;
+
+    const [responseState, setResponseState] = useState({
         visible: false, record: {}
     });
     const [imageState, setImageState] = useState({
@@ -22,26 +31,57 @@ export default function ActivityReportContainer() {
     const [filterVisible, setFilterVisible] = useState(false);
 
     const showFilterModal = () => setFilterVisible(true);
-    const showReportModal = report => {
+    const showReportModal = record => {
         setReportVisible(true);
+
+        const {report} = record;
         setRecord(report);
     };
 
-    const activityProps = {
-        reportVisible, setReportVisible, record, 
-        showReportModal, showFilterModal,
-        filterVisible, setFilterVisible,
-        setRespState, setImageState,
-        activities: store.narratives
+    // on report delete 
+    const onDelete = key => {
+        Api.narrative.delete(key)
+        .then(res => fetchNarratives(dispatch));
     };
-    const responseProps = {respState, setRespState};
+
+    // persist state on page reload
+    // should be implemented through the global state
+    // but only working using the back button on Response and 
+    // ReportImage page
+    useEffect(() => {
+        const storeReportKey = sessionStorage['reportKey'];
+        const imageState = sessionStorage['imageState'];
+
+        if (storeReportKey) {
+            activityLoop:
+            for (const activity of activities) {
+                const {report: reportList} = activity;
+                for (const report of reportList) {
+                    if (report.key === Number(storeReportKey)) {
+                        if (imageState) {
+                            setImageState({visible: true, record: report})
+                        } else {
+                            setResponseState({visible: true, record: report});
+                        }
+                        break activityLoop;                        
+                    }
+                }
+            }
+        }
+    }, [activities]);
+
+    const activityProps = {
+        reportVisible, setReportVisible, record, showReportModal, 
+        showFilterModal, filterVisible, setFilterVisible,
+        setResponseState, setImageState, onDelete,
+        activities
+    };
+    const responseProps = {responseState, setResponseState};
     const imageProps = {imageState, setImageState};
 
     return (
-        respState.visible ? 
-        <ResponseContainer {...responseProps} /> :
-        imageState.visible ? 
-        <ReportImageContainer {...imageProps} /> :    
+        responseState.visible ? <ResponseContainer {...responseProps} /> :
+        imageState.visible ? <ReportImageContainer {...imageProps} /> :    
         <ActivityReport {...activityProps} />
     );
 }
