ffb4278c477d71420fc2f5dfcf6d80ecdf489109
Dohd <alloicebrian@gmail.com>
2021-10-24 22:56:12 +0300

    Add caseStudy modal

--- src/pages/activity_report/Response.jsx
+++ src/pages/activity_report/Response.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Button, Card, Space, Table } from 'antd';
-import { ArrowLeftOutlined, FilePdfOutlined } from '@ant-design/icons';
+import { Button, Card, Space, Table, Modal, Input, Form } from 'antd';
+import { ArrowLeftOutlined, FilePdfOutlined, EditOutlined } from '@ant-design/icons';
 
 import { components, editableColumns } from './EditableTable';
 
 export default function Response(props) {
     const {
         toggleReportView, columns, dataSource, caseStudy,
-        onExport
+        onExport, visible, setVisible, onOk, form
     } = props;
 
     return (
@@ -34,9 +34,35 @@ export default function Response(props) {
             }
         >
             <div>
-                <p>Case study</p>
-                <p>&bull;&nbsp;{caseStudy}</p>
+                <div style={{padding: '0 0 1em 1em'}}>
+                    <span>Case study</span>
+                    <Button
+                        type='default' 
+                        onClick={() => setVisible(true)}
+                        icon={<EditOutlined />}
+                        size='small'
+                        style={{marginLeft: '.5em'}}
+                    />
+                </div>                
+                <ul><li>{caseStudy}</li></ul>
             </div>
+            <Modal
+                title='Edit Case study'
+                visible={visible}
+                onOk={onOk}
+                okText='Update'
+                onCancel={() => setVisible(false)}
+            >
+                <Form form={form} initialValues={{caseStudy}}>
+                    <Form.Item
+                        name="caseStudy"
+                        label="Case Study"
+                        required={true}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
             
             <div style={{overflowX: 'auto'}}>
                 <Table
--- src/pages/activity_report/ResponseContainer.jsx
+++ src/pages/activity_report/ResponseContainer.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'antd';
 
+import Api from 'api';
 import createPdf, { table } from 'utils/pdfMake'; 
 
 import Response from './Response';
 
 export default function ResponseContainer(props) {
-    const {responseState, setResponseState} = props;
+    const {responseState, setResponseState, fetchNarratives} = props;
 
     const toggleReportView = () => {
         setResponseState(prev => ({
@@ -74,29 +76,46 @@ export default function ResponseContainer(props) {
         });
 
         const colCount = columns.length + 1;
-        const colHeader = columns.map(({title}) => title);
-
         const data = table.data(cells, colCount);
 
         let header1 = table.header(['Question', 'Task Response']);
         for (let i = 2; i < colCount; i++) {
             header1 = header1.concat(['']);
         }
+        // add rowSpan and colSpan to index 0 and 1 objects
         header1.splice(0, 1, {...header1[0], rowSpan: 2});
         header1.splice(1, 1, {...header1[1], colSpan: columns.length});
 
+        const colHeader = columns.map(({title}) => title);
         const header2 = table.header(colHeader);
         header2.splice(0, 0, '');
 
         const body = table.body(header1, header2, ...data);
-        // return console.log(body);
         const content = [{text: 'Case Study', style: 'subheader'}, { ul: [caseStudy] }];
         createPdf('Narrative Report', body, void 0, 'landscape', 2, void 0, void 0, content);
     };
 
+    // modal logic
+    const [visible, setVisible] = useState(false);
+    const onCreate = values => {
+        setVisible(false);
+        const reportKey = responseState['record']['key'];
+        // api call
+        Api.caseStudy.patch(reportKey, values)
+        .then(res => fetchNarratives())
+    };
+
+    const [form] = Form.useForm();
+    const onOk = () => {
+        form.validateFields()
+        .then(values => onCreate(values))
+        .catch(console.log);
+    };
+
     const params = {
         toggleReportView, dataSource,
-        columns, caseStudy, onExport
+        columns, caseStudy, onExport,
+        visible, setVisible, onOk, form,
     };
     return <Response {...params} />;
 }
